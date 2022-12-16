294789715450a596d843e7dc92d22a8be5ee201c
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2022-12-15 23:56:57 -0500

    NVIM: switch to lsp-zero

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,149 +1,35 @@
 --=====================================================
 -- nvim-cmp Settings
 --=====================================================
+local lsp = require('lsp-zero')
 
-local cmp = require 'cmp'
-
-cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-j>'] = cmp.mapping.select_next_item(),
-		['<C-k>'] = cmp.mapping.select_prev_item(),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
-	}),
-	sources = cmp.config.sources({
-		-- the order of the sources matters
-		{ name = 'nvim_lua' },
-		{ name = 'nvim_lsp' },
-		{ name = 'path' },
-		{ name = 'luasnip' },
-		{ name = 'buffer' }
-	}),
-	experimental = {
-		native_menu = false,
-		ghost_text = true,
-	},
+lsp.preset('recommended')
+lsp.ensure_installed({
+	'pyright',
+	'sumneko_lua',
+	'rust_analyzer',
 })
 
--- Set up lspconfig
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+lsp.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
 
--- add to your shared on_attach callback
-local on_attach = function(client, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+	vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+	vim.keymap.set("i", "<C-k>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, opts)
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wl', function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
-end
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+end)
 
-
---=====================================================
--- Python Settings
---=====================================================
-
-require('lspconfig')['pyright'].setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-}
-
-
---=====================================================
--- Lua Settings
---=====================================================
-
-require('lspconfig')['sumneko_lua'].setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = 'LuaJIT',
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { 'vim' },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-}
-
-
---=====================================================
--- TypeScript Settings
---=====================================================
-
-require('lspconfig')['tsserver'].setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	-- flags = lsp_flags,
-}
-
-
---=====================================================
--- Rust Settings
---=====================================================
-
-require('lspconfig')['rust_analyzer'].setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = {
-		["rust-analyzer"] = {
-			imports = {
-				granularity = {
-					group = "module",
-				},
-				prefix = "self",
-			},
-			cargo = {
-				buildScripts = {
-					enable = true,
-				},
-			},
-			procMacro = {
-				enable = true
-			},
-		}
-	}
-}
+lsp.setup()
 
 
 --=====================================================
