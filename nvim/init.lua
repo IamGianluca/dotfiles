1afeb3bbebd95acb5f8f4d1224cd5c3ab65c99a2
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2022-12-24 18:13:15 -0500

    NVIM: move settings in set.lua and enable moving block of code in visual mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,51 +2,7 @@
 -- Load Plugins
 --=====================================================
 
+require('set')
 require('remap')
 require('plugins')
 require('theme')
-
---=====================================================
--- General Settings
---=====================================================
-
--- open new splits on the right or on the bottom of the screen
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
--- prevent common typos when closing/saving
-vim.cmd.abbrev('W!', 'w!')
-vim.cmd.abbrev('Q!', 'q!')
-vim.cmd.abbrev('Qall!', 'qall!')
-vim.cmd.abbrev('Wq', 'wq')
-vim.cmd.abbrev('Wa', 'wa')
-vim.cmd.abbrev('wQ', 'wq')
-vim.cmd.abbrev('WQ', 'wq')
-vim.cmd.abbrev('W', 'w')
-vim.cmd.abbrev('Q', 'q')
-vim.cmd.abbrev('Qa', 'qa')
-vim.cmd.abbrev('Qall', 'qall')
-
--- directories and swap files
-vim.opt.backup = false
-vim.opt.swapfile = false
-
--- search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-
---=====================================================
--- Visual Settings
---=====================================================
-
--- display relative line numbers
-vim.opt.ruler = true
-vim.opt.relativenumber = true
-vim.wo.number = true
-
--- extra goodies
-vim.wo.wrap = false -- don't wrap lines
-vim.opt.signcolumn = 'auto'
-vim.opt.colorcolumn = '80'
-vim.opt.cmdheight = 0
