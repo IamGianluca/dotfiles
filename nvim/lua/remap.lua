c432d116f4d67f79f7bccd905355b1b9489c626d
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2022-12-25 14:47:41 -0500

    NVIM: copy some remaps from ThePrimeagen (#41)

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -17,3 +17,22 @@ vim.keymap.set("n", "<Esc>", ":noh<CR><Esc>", { silent = true, desc = "Clean sea
 -- when in visual mode, move up/down block of selected code
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- when using J, do not move cursor
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- when using half-page jumping, keep cursor in the middle of the buffer
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keep search result in the middle of the buffer
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- when copy-pasting, do not put replaced word into registry
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- copy to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
