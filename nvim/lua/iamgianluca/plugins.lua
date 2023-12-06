249e1e91f7e05a5839622a9769c412153b4a3e97
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-12-06 11:39:23 -0500

    feat(nvim): bump telescope version from 0.1.3 to 0.1.5

--- nvim/lua/iamgianluca/plugins.lua
+++ nvim/lua/iamgianluca/plugins.lua
@@ -29,7 +29,7 @@ return require("lazy").setup({
 	-- project navigation
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.3",
+		tag = "0.1.5",
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" }
 	},
 
