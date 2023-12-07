0d68d4cb53c109c67d711417d082617585030a79
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-12-06 23:41:05 -0500

    feat(nvim): change keymaps to navigate between luasnippet fields

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -8,13 +8,10 @@ ls.config.set_config {
 	-- this tells luasnip to remember to keep around the last snippet.
 	-- you can jump back into it even if you move outside of the selection
 	history = true,
-
 	-- this one is cool cause if you have dynamic snippets, it updates as you type!
 	updateevents = "TextChanged,TextChangedI",
-
 	-- autosnippets:
 	enable_autosnippets = true,
-
 	ext_opts = {
 		[types.choiceNode] = {
 			active = {
@@ -24,33 +21,16 @@ ls.config.set_config {
 	},
 }
 
--- keymaps
--- <c-k> is my expansion key
--- this will expand the current item or jump to the next item within the snippet.
-vim.keymap.set({ "i", "s" }, "<c-k>", function()
-	if ls.expand_or_jumpable() then
-		ls.expand_or_jump()
-	end
-end, { silent = true })
+vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-H>", function() ls.jump(-1) end, { silent = true })
 
--- <c-j> is my jump backwards key.
--- this always moves to the previous item within the snippet
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
-	if ls.jumpable(-1) then
-		ls.jump(-1)
-	end
-end, { silent = true })
-
--- <c-l> is selecting within a list of options.
--- this is useful for choice nodes (introduced in the forthcoming episode 2)
-vim.keymap.set("i", "<c-l>", function()
+vim.keymap.set({ "i", "s" }, "<C-E>", function()
 	if ls.choice_active() then
 		ls.change_choice(1)
 	end
-end)
-
+end, { silent = true })
 vim.keymap.set("i", "<c-u>", require "luasnip.extras.select_choice")
-
 -- shorcut to source my luasnips file again, which will reload my snippets
 vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
 
