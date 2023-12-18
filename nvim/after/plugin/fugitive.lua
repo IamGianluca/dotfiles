bc21b8121cc90070e2ffd455dfa1f86b78e7bf43
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-12-17 21:52:03 -0500

    docs(nvim): document keymaps for solving merge conflicts in :G(v)diffsplit

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -2,6 +2,6 @@
 -- vim-fugitive Settings
 --=====================================================
 
--- when in mergediff, use `gu` to accept left side, and `gh` to accept right side
-vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+-- keymap for easier resolving merge conflicts in :G(v)diffsplit
+vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>") -- get changes from target branch
+vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>") -- get changes from merge branch
