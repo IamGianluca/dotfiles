1610e34cdd02bed702f0a08054c0ec641f76c42e
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-12-30 14:41:04 -0500

    fix(dwm): make feh running in background

--- dwm-flexipatch/autostart.sh
+++ dwm-flexipatch/autostart.sh
@@ -1,4 +1,4 @@
-feh --bg-scale ~/Dropbox/ubuntu-mantic-wallpaper.png
+feh --bg-scale ~/Dropbox/ubuntu-mantic-wallpaper.png &
 dropbox start &
 dwmblocks &
 redshift -l 41:-74 -t 5700:3600 -g 0.8 -m randr -v &
