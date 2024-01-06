f18b4daf6634eaa185584c9bab1a248b2af5d272
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-01-05 20:28:49 -0500

    docs(dwmblocks): document how to debug dwmblocks

--- dwm-flexipatch/autostart.sh
+++ dwm-flexipatch/autostart.sh
@@ -2,6 +2,9 @@ feh --bg-scale ~/Dropbox/ubuntu-mantic-wallpaper.png &
 
 dropbox start &
 
+# the `export` command is required to source the binary executable needed to fill
+# the dwmblocks fields. To debug, run the following command to redirect the std to 
+# a log file: 
 # export PATH=$PATH:$HOME/.local/bin/statusbar/ && dwmblocks 2>/tmp/dwmblocks.log &
 export PATH=$PATH:$HOME/.local/bin/statusbar/ && dwmblocks &
 
