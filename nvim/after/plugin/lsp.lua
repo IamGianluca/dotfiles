acbf6051811611439b187adef15f43930e727dbb
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-01-08 16:09:31 -0500

    chore(nvim): restore our usual keymappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,9 +5,18 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({ buffer = bufnr })
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+	vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 -- enable automatic installation and setup of language servers
@@ -16,11 +25,15 @@ require('mason-lspconfig').setup({
 	ensure_installed = { "lua_ls", "rust_analyzer", "pyright" },
 	handlers = {
 		lsp_zero.default_setup,
+		lua_ls = function()
+			local lua_opts = lsp_zero.nvim_lua_ls()
+			require('lspconfig').lua_ls.setup(lua_opts)
+		end,
 		rust_analyzer = lsp_zero.noop, -- exclude rust_analyze from autoconfiguration, required by rustaceanvim
 	},
 })
 
--- configure completion sources
+-- configure nvim-cmp
 local cmp = require('cmp')
 local cmp_format = require('lsp-zero').cmp_format()
 local cmp_action = require('lsp-zero').cmp_action()
@@ -41,23 +54,9 @@ cmp.setup({
 		completeopt = 'menu,menuone,noinsert',
 	},
 	mapping = cmp.mapping.preset.insert({
-		-- confirm completion item
 		['<CR>'] = cmp.mapping.confirm({ select = false }),
-
-		-- toggle completion menu
-		['<C-e>'] = cmp_action.toggle_completion(),
-
-		-- tab complete
 		['<Tab>'] = cmp_action.tab_complete(),
 		['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-		-- navigate between snippet placeholder
-		['<C-d>'] = cmp_action.luasnip_jump_forward(),
-		['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-		-- scroll documentation window
-		['<C-f>'] = cmp.mapping.scroll_docs(-5),
-		['<C-d>'] = cmp.mapping.scroll_docs(5),
 	}),
 	window = {
 		documentation = cmp.config.window.bordered(),
@@ -65,12 +64,14 @@ cmp.setup({
 	formatting = cmp_format,
 })
 
+
 --=====================================================
 -- conform Settings
 --=====================================================
 
 -- needed to format and sort imports in Python since PyRight doesn't offer
--- those functionalities; expects black and usort to be available in PATH
+-- those functionalities; expects black and usort to be available in PATH.
+-- We can accomplish that by installing both tools via Mason
 require("conform").setup({
 	formatters_by_ft = {
 		python = { "usort", "black" },
