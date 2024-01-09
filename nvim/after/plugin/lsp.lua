850ccbaa4627dc5080572379ba08a0985eee0c36
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-01-09 11:43:13 -0500

    chore(nvim): remove duplicate setting of a local variable

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -100,7 +100,6 @@ require('nvim-autopairs').setup()
 
 -- insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
 cmp.event:on(
 	'confirm_done',
 	cmp_autopairs.on_confirm_done()
