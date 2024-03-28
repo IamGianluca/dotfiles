cc8215c113956153b6a05f9e8e9f510e84c75c22
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-03-28 08:03:47 -0400

    fix(nvim): missing dependencies for nvim-dap-ui was throwing an error

--- nvim/lua/iamgianluca/plugins.lua
+++ nvim/lua/iamgianluca/plugins.lua
@@ -52,7 +52,7 @@ return require("lazy").setup({
 	-- debugging
 	{ "mfussenegger/nvim-dap" },
 	{ "jay-babu/mason-nvim-dap.nvim" },
-	{ "rcarriga/nvim-dap-ui" },
+	{ "rcarriga/nvim-dap-ui",                     dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
 
 	-- snippets
 	{
