db9a1dc3d92d570a255fe30326e7ce9021113bc1
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-04-23 20:17:01 -0400

    docs(nvim): improve documentation for keymaps to navigate between windows

--- nvim/lua/iamgianluca/remap.lua
+++ nvim/lua/iamgianluca/remap.lua
@@ -10,10 +10,10 @@ vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Start netrw file explorer" })
 
 -- Navigate between windows
-vim.keymap.set("", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
-vim.keymap.set("", "<C-k>", "<C-w>k", { desc = "Move to higher window" })
-vim.keymap.set("", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move focus to the right window" })
 
 -- Navigate between quickfix items
 vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz", { desc = "Previous item in quickfix list" })
