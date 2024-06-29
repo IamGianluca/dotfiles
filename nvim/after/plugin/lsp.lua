08d3e07c875fdf523bb1558a7c340eac044120b3
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-06-28 20:37:57 -0400

    feat(lsp): enable inlay hints

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -134,6 +134,8 @@ o.spell = true
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
 	vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true })
 
+-- Show inlay hints
+vim.lsp.inlay_hint.enable(true, { 0 })
 
 -- --=====================================================
 -- -- rust-tools Settings
