baf5de434312f5f5e3acee6c7e5e3cbc15cfd76b
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-09-01 22:50:04 -0400

    feat(nvim): upgrade telescope (0.1.5 -> 0.1.8)

--- nvim/lua/iamgianluca/plugins.lua
+++ nvim/lua/iamgianluca/plugins.lua
@@ -24,7 +24,7 @@ return require("lazy").setup({
 	-- Project navigation
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
+		tag = "0.1.8",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
