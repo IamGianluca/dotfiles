4732b7f23a8d1ba1dfb2126eabbcbc89f3fc00e6
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-09-29 11:01:13 -0400

    feat(nvim): disable default <C-h> and <C-l> keymaps in oil.nvim to preserve custom buffer navigation keymaps

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -6,6 +6,10 @@ return {
 		config = function()
 			require("oil").setup({
 				default_file_explorer = true,
+				keymaps = {
+					["<C-h>"] = false,
+					["<C-l>"] = false,
+				},
 				vim.keymap.set("n", "<leader>e", ":Oil<CR>", { desc = "Start oil.nvim" }),
 			})
 		end,
