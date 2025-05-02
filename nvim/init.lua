855205006f3329f69c4a94530e245db59471521f
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2025-05-01 23:49:23 -0400

    fix(nvim): setup `mapleader` and `maplocalleader` before loading lazy.nvim so that mappings are correct

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,47 @@
 --=====================================================
+-- Key Remappings
+--=====================================================
+
+-- Set <Space> as the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Navigate between windows
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move focus to the right window" })
+
+-- Navigate between quickfix items
+vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz", { desc = "Previous item in quickfix list" })
+vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz", { desc = "Next item in quickfix list" })
+
+-- Navigate between location list items
+vim.keymap.set("n", "[l", "<cmd>lprev<CR>zz", { desc = "Previous item in location list" })
+vim.keymap.set("n", "]l", "<cmd>lnext<CR>zz", { desc = "Next item in location list" })
+
+-- When in visual mode, move up/down block of selected code
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- When using J, do not move cursor
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- When using half-page jumping, keep cursor in the middle of the buffer
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep search result in the middle of the buffer
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- When copy-pasting, do not put replaced word into registry
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- Clear search highlight on pressing <Esc> in Normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { silent = true, desc = "Clean search highlight" })
+
+--=====================================================
 -- General Settings
 --=====================================================
 
@@ -76,49 +119,6 @@ vim.opt.scrolloff = 999 -- Keep cursor always in the middle
 vim.opt.virtualedit = "block" -- Allow cursor to be positioned where there is no actual character in Visual Block mode
 
 vim.g.border_style = "rounded" ---@type "single"|"double"|"rounded"
-
---=====================================================
--- Key Remappings
---=====================================================
-
--- Set <Space> as the leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Navigate between windows
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move focus to the upper window" })
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move focus to the right window" })
-
--- Navigate between quickfix items
-vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz", { desc = "Previous item in quickfix list" })
-vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz", { desc = "Next item in quickfix list" })
-
--- Navigate between location list items
-vim.keymap.set("n", "[l", "<cmd>lprev<CR>zz", { desc = "Previous item in location list" })
-vim.keymap.set("n", "]l", "<cmd>lnext<CR>zz", { desc = "Next item in location list" })
-
--- When in visual mode, move up/down block of selected code
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
--- When using J, do not move cursor
-vim.keymap.set("n", "J", "mzJ`z")
-
--- When using half-page jumping, keep cursor in the middle of the buffer
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
--- Keep search result in the middle of the buffer
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- When copy-pasting, do not put replaced word into registry
-vim.keymap.set("x", "<leader>p", '"_dP')
-
--- Clear search highlight on pressing <Esc> in Normal mode
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { silent = true, desc = "Clean search highlight" })
 
 --=====================================================
 -- Other Settings
