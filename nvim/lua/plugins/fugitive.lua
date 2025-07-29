81f4729b8007da99e2b18b5db61026cd7033edf6
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2025-07-29 11:03:07 -0400

    feat: remove fugitive

--- nvim/lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"tpope/vim-fugitive",
-	config = function()
-		-- Keymap for easier resolving merge conflicts in :G(v)diffsplit
-		vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>") -- Get changes from target branch
-		vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>") -- Get changes from merge branch
-	end,
-}
