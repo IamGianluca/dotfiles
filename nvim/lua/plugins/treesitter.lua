d0d58d746cbbe4c8d9cbf47fe8c90cf86b1ab20f
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2026-01-22 21:00:43 -0500

    feat: migrate to main branches of nvim-treesitter and nvim-treesitter-textobjects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,99 +1,140 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
+		branch = "main",
 		build = ":TSUpdate",
-		dependencies = {
-			{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
+	},
+	{
+		"MeanderingProgrammer/treesitter-modules.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "rust", "zig" },
+			ignore_install = {},
+			modules = {},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+			},
+			fold = {
+				enable = true,
+			},
 		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "rust" },
-				ignore_install = {},
-				modules = {},
-				sync_install = false,
-				auto_install = true,
-				highlight = {
+			require("nvim-treesitter-textobjects").setup({
+
+				select = {
 					enable = true,
-					additional_vim_regex_highlighting = false,
+					lookahead = true,
+					include_surrounding_whitespace = true,
 				},
-				indent = {
+				move = {
 					enable = true,
+					set_jumps = true,
 				},
-				fold = {
+				swap = {
 					enable = true,
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter region" },
-							["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter region" },
-							["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
-							["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
-							["ac"] = { query = "@class.outer", desc = "Select the outer part of a class region" },
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						},
-						include_surrounding_whitespace = true,
-					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]a"] = { query = "@parameter.inner", desc = "Next parameter start" },
-							["]m"] = { query = "@function.outer", desc = "Next function start" },
-							["]]"] = { query = "@class.outer", desc = "Next class start" },
-						},
-						goto_next_end = {
-							["]M"] = { query = "@function.outer", desc = "Next function end" },
-							["]["] = { query = "@class.outer", desc = "Next class end" },
-						},
-						goto_previous_start = {
-							["[a"] = { query = "@parameter.inner", desc = "Previous parameter start" },
-							["[m"] = { query = "@function.outer", desc = "Previous function start" },
-							["[]"] = { query = "@class.outer", desc = "Previous class start" },
-						},
-						goto_previous_end = {
-							["[M"] = { query = "@function.outer", desc = "Previous function end" },
-							["[["] = { query = "@class.outer", desc = "Previous class end" },
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = {
-							["<leader>a"] = "@parameter.inner",
-						},
-						swap_previous = {
-							["<leader>A"] = "@parameter.inner",
-						},
-					},
 				},
 			})
+
+			-- Set keymaps for textobjects selection
+			local select = require("nvim-treesitter-textobjects.select").select_textobject
+
+			vim.keymap.set({ "x", "o" }, "aa", function()
+				select("@parameter.outer", "textobjects")
+			end, { desc = "Select outer part of a parameter region" })
+			vim.keymap.set({ "x", "o" }, "ia", function()
+				select("@parameter.inner", "textobjects")
+			end, { desc = "Select inner part of a parameter region" })
+			vim.keymap.set({ "x", "o" }, "af", function()
+				select("@function.outer", "textobjects")
+			end, { desc = "Select outer part of a function region" })
+			vim.keymap.set({ "x", "o" }, "if", function()
+				select("@function.inner", "textobjects")
+			end, { desc = "Select inner part of a function region" })
+			vim.keymap.set({ "x", "o" }, "ac", function()
+				select("@class.outer", "textobjects")
+			end, { desc = "Select the outer part of a class region" })
+			vim.keymap.set({ "x", "o" }, "ic", function()
+				select("@class.inner", "textobjects")
+			end, { desc = "Select inner part of a class region" })
+
+			-- Set keymaps for textobjects swap
+			vim.keymap.set("n", "<leader>a", function()
+				require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
+			end, { desc = "Swap current argument with next one" })
+			vim.keymap.set("n", "<leader>A", function()
+				require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner")
+			end, { desc = "Swap current argument with previous one" })
+
+			-- Set keymaps for textobjects move
+			local prev_start = require("nvim-treesitter-textobjects.move").goto_previous_start
+			vim.keymap.set({ "n", "x", "o" }, "[a", function()
+				prev_start("@parameter.inner", "textobjects")
+			end, { desc = "Previous parameter start" })
+			vim.keymap.set({ "n", "x", "o" }, "[m", function()
+				prev_start("@function.outer", "textobjects")
+			end, { desc = "Previous function start" })
+			vim.keymap.set({ "n", "x", "o" }, "[c", function()
+				prev_start("@class.outer", "textobjects")
+			end, { desc = "Previous class start" })
+
+			local next_start = require("nvim-treesitter-textobjects.move").goto_next_start
+			vim.keymap.set({ "n", "x", "o" }, "]a", function()
+				next_start("@parameter.inner", "textobjects")
+			end, { desc = "Next parameter start" })
+			vim.keymap.set({ "n", "x", "o" }, "]m", function()
+				next_start("@function.outer", "textobjects")
+			end, { desc = "Next function start" })
+			vim.keymap.set({ "n", "x", "o" }, "]c", function()
+				next_start("@class.outer", "textobjects")
+			end, { desc = "Next class start" })
+
+			local prev_end = require("nvim-treesitter-textobjects.move").goto_previous_end
+			vim.keymap.set({ "n", "x", "o" }, "[M", function()
+				prev_end("@function.outer", "textobjects")
+			end, { desc = "Previous function end" })
+			vim.keymap.set({ "n", "x", "o" }, "[C", function()
+				prev_end("@class.outer", "textobjects")
+			end, { desc = "Previous class end" })
+
+			local next_end = require("nvim-treesitter-textobjects.move").goto_next_end
+			vim.keymap.set({ "n", "x", "o" }, "]M", function()
+				next_end("@function.outer", "textobjects")
+			end, { desc = "Next function end" })
+			vim.keymap.set({ "n", "x", "o" }, "]C", function()
+				next_end("@class.outer", "textobjects")
+			end, { desc = "Next class end" })
 		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			require("treesitter-context").setup({
-				enable = true,
-				max_lines = 0,
-				trim_scope = "outer",
-				patterns = {
-					default = {
-						"class",
-						"function",
-						"method",
-					},
+		opts = {
+			enable = true,
+			max_lines = 0,
+			trim_scope = "outer",
+			patterns = {
+				default = {
+					"class",
+					"function",
+					"method",
 				},
-				exact_patterns = {
-					-- rust = true,
-				},
-				zindex = 20,
-				mode = "cursor",
-				separator = nil,
-			})
-		end,
+			},
+			exact_patterns = {
+				-- rust = true,
+			},
+			zindex = 20,
+			mode = "cursor",
+			separator = nil,
+		},
 	},
 	{
 		-- Better incremental selection compared to the one provided by nvim-treesitter
