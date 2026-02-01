438315416c57c22e2530cb04ddab89c9711a8289
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2026-01-31 19:56:05 -0500

    docs: added a comment to remember to switch back to upstream repo once fix is merged upstream

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,4 +1,9 @@
 return {
+	-- NOTE: I'm currently maintaining a fork of this plugin, to fix a warning
+	-- that shows up when starting neovim. There is an open PR in the upstream repo
+	-- which implements the same changes I made. Once the maintainer accepts the
+	-- changes, we can switch to using the upstream repo.
+	-- Link to the PR: https://github.com/norcalli/nvim-colorizer.lua/pull/115
 	-- "norcalli/nvim-colorizer.lua",
 	"iamgianluca/nvim-colorizer.lua",
 	event = "BufEnter",
