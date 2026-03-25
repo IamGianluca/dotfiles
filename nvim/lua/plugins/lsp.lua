d61fba4a45a933dddfbb9a7392692b173e525bbe
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2026-03-24 21:29:32 -0400

    feat(nvim): add border around popup windows for signature help and hover

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,7 +14,7 @@ return {
 						vim.lsp.buf.definition()
 					end, opts)
 					vim.keymap.set("n", "K", function()
-						vim.lsp.buf.hover()
+						vim.lsp.buf.hover({ border = "rounded" })
 					end, opts)
 					vim.keymap.set("n", "<leader>vws", function()
 						vim.lsp.buf.workspace_symbol()
@@ -38,7 +38,7 @@ return {
 						vim.lsp.buf.rename()
 					end, opts)
 					vim.keymap.set("i", "<C-h>", function()
-						vim.lsp.buf.signature_help()
+						vim.lsp.buf.signature_help({ border = "rounded" })
 					end, opts)
 				end,
 			})
